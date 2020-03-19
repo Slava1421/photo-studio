4ab7939bb709eb0c5909dd23dc4f71ddbfb3dce1
Slava142192 <slava1421@gmail.com>
2020-03-19 21:35:01 +0200

    add components and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from 'src/modules/home-page/components/home-page/home-page.component';
+import { GalleryPageComponent } from 'src/modules/gallery-page/components/gallery-page/gallery-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'gallery', component: GalleryPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
