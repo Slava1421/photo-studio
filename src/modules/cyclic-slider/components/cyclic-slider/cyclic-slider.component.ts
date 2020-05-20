c6d20b15d8e01760d4cbd5f886582a4995e91d59
Slava142192 <slava1421@gmail.com>
2020-05-20 17:14:02 +0300

    cyclic-slider: add component

---
+++ src/modules/cyclic-slider/components/cyclic-slider/cyclic-slider.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cyclic-slider',
+  templateUrl: './cyclic-slider.component.html',
+  styleUrls: ['./cyclic-slider.component.scss']
+})
+export class CyclicSliderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/modules/cyclic-slider/phc-cyclic-slider.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CyclicSliderComponent } from './components/cyclic-slider/cyclic-slider.component';
+
+
+
+@NgModule({
+  declarations: [CyclicSliderComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CyclicSliderModule { }
